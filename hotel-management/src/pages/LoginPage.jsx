201f7f91e1176966a6def8f532dcb25b77333773
EdrinM22 <emolla22@epoka.edu.al>
2024-03-28 18:38:02 +0100

    fixed login for phone

--- hotel-management/src/pages/LoginPage.jsx
+++ hotel-management/src/pages/LoginPage.jsx
@@ -1,8 +1,11 @@
 import Login from "../components/Login";
+import "../components/Login.css";
 export default function LoginPage() {
     return (
         <>
+        <main className="login-main">
             <Login/>
+        </main>
         </>
         
 
