a05e2a33173ef705c50acf99873824fcb02e7d1c
IlviCumani <ilviCumani@gmail.com>
2024-03-29 16:10:13 +0100

    Added the logic for submitting the feedback form

---
+++ hotel-management/src/components/Button.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import "./Button.css"
+
+export default function Button({ children, display = 'primary', onClick, ...props }) {
+    const btnClassname = {
+        primary: 'primary_btn',
+        secondary: 'secondary_btn',
+        danger: 'danger_btn'
+    }
+
+
+    return (
+        <button className={btnClassname[display]} onClick={onClick} {...props}>
+            {children}
+        </button>
+    );
+}
