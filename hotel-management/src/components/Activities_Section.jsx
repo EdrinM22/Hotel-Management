76c1f26173914f63efea4b0854d5395ab1c11645
mateo <mgjini21@epoka.edu.al>
2024-05-06 22:55:30 +0200

    mainpage

---
+++ hotel-management/src/components/Activities_Section.jsx
@@ -0,0 +1,9 @@
+import "./Activities_Section.css"
+const Activities = () => {
+    return(
+        <section className="activities">
+            <h1>Activities</h1>
+        </section>
+    )
+}
+export default Activities
---
+++ hotel-management/src/components/Rooms_Section.jsx
@@ -0,0 +1,15 @@
+import "./Rooms_Section.css"
+import Room_Card from "./Room_Card.jsx";
+
+const Rooms = () => {
+    return(
+        <section className="rooms">
+            <h1>Rooms & Suites</h1>
+            <Room_Card />
+            <Room_Card />
+            <Room_Card />
+            <Room_Card />
+        </section>
+    )
+}
+export default Rooms
