c31dd7a5fec9976d9dba3a0736bd899420712a24
mateo <mgjini21@epoka.edu.al>
2024-05-17 18:24:23 +0200

    bugs fixed

--- hotel-management/src/components/Receipt.jsx
+++ hotel-management/src/components/Receipt.jsx
@@ -19,23 +19,27 @@ const Receipt = () => {
                     <p>Guests: </p>
                     <p>Nr of guests</p>
                 </div>
-
-                <div className="room-type">
-                    <h1>Standard Room </h1>
-                    <h3>$499</h3>
-                </div>
-                <p>Breakfast included</p>
-                <p>10 nights</p>
-                <div className="receipt-edit">
-                    <Button display={"text"}> <span style={{fontSize: "1.5rem"}}>&#9998;</span> Edit</Button>
-                    <Button display={"text"}> <span style={{fontSize: "1.5rem"}}>&#128465;</span> Remove</Button>
+                <div>
+                    <div className="room-type">
+                        <h1>Standard Room </h1>
+                        <h3>$499</h3>
+                    </div>
+                    <p>Breakfast included</p>
+                    <p>10 nights</p>
+                    <div className="receipt-edit">
+                        <Button display={"text"}> <span style={{fontSize: "1.5rem"}}>&#9998;</span> Edit</Button>
+                        <Button display={"text"}> <span style={{fontSize: "1.5rem"}}>&#128465;</span> Remove</Button>
+                    </div>
                 </div>
             </div>
             <div className="receipt-total">
                 <p>Add room</p>
                 <p>Total: $499</p>
             </div>
-            <Button display={"primary"} id="book-now">Book now</Button>
+            <div id="book-now">
+                <Button display={"primary"} >Book now</Button>
+            </div>
+
         </div>
     )
 }
